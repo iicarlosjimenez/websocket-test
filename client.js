5e46c73e94ac725bd9df682336531f839ef412cf
Carlos Jiménez <iicarlosjim@gmail.com>
2025-08-15 12:07:40 -0600

    feat: send Question

--- client.js
+++ client.js
@@ -1,15 +1,18 @@
-import WebSocket from "ws";
+const WebSocket = require("ws");
 
 const host = "localhost";
-const port = 8000
+const port = 8080
 const wsc = new WebSocket(`ws://${host}:${port}`)
+const questions = require("./questions.json");
 
 wsc.on('error', console.error);
 
 wsc.on("open", function open() {
     console.log("Conectado al servidor websocket");
+
+    const question = questions.find(qs => qs.category == "geek")
     
-    wsc.send(JSON.stringify({ tipo: "saludo", mensaje: "Hola desde el cliente!" }));
+    wsc.send(JSON.stringify(question));
 })
 
 wsc.on("message", function message(data) {
